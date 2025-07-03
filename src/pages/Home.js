488c483d66910e029a7adf50d171409039e6fb70
khalifa <khalifadalhat@gmail.com>
2025-07-03 21:21:44 +0100

    Update Home component title and improve formatting; changed restaurant name to 'Savory Haven' and adjusted JSX structure for consistency.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import '../styles/Home.css'
-import { Link } from 'react-router-dom';
-import BannerImage from '../assets/images/dinner.jpg'
+import React from "react";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/images/dinner.jpg";
 
 function Home() {
   return (
-    <div className='home'style={{backgroundImage: `url(${BannerImage})`}}>
-        <div className="headerContainer" >
-            <h1>Khalifa's Restaurant</h1>
-            <p>Satisfy your cravings!</p>
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1>Savory Haven Restaurant</h1>
+        <p>Satisfy your cravings!</p>
 
-            <Link to='/menu'>
-            <button>Order Now</button>
-            </Link>
-        </div>
+        <Link to="/menu">
+          <button>Order Now</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
