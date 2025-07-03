855b4b19e59c0e23af62ed0d71863582949ce307
khalifa <khalifadalhat@gmail.com>
2025-07-03 21:21:13 +0100

    Refactor NavBar component to improve responsiveness and accessibility; added close icon for mobile menu toggle.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,31 +3,41 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/images/lunch.jpg";
 import "../styles/NavBar.css";
 import ReorderIcon from "@mui/icons-material/Reorder";
+import CloseIcon from "@mui/icons-material/Close";
 
 function NavBar() {
   const [openLinks, setOpenLinks] = useState(false);
 
-  const toggleNavbar = () => [setOpenLinks(!openLinks)];
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className="navbar">
       <div className="leftSide">
-        <div id={openLinks ? "open" : "close"}>
-          <img src={Logo} alt="Logo" />
-          <div className="hiddenLinks">
-            <Link to="/">Home</Link>
-            <Link to="/menu">Menu</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-          </div>
-        </div>
+        <Link to="/" className="logo-link">
+          <img src={Logo} alt="Savory Haven Logo" className="logo" />
+          <span className="logo-text">Savory Haven</span>
+        </Link>
       </div>
+
       <div className="rightSide">
-        <Link to="/">Home</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <button onClick={toggleNavbar}>
-          <ReorderIcon />
+        <div className={`nav-links ${openLinks ? "active" : ""}`}>
+          <Link to="/" onClick={toggleNavbar}>
+            Home
+          </Link>
+          <Link to="/menu" onClick={toggleNavbar}>
+            Menu
+          </Link>
+          <Link to="/about" onClick={toggleNavbar}>
+            About
+          </Link>
+          <Link to="/contact" onClick={toggleNavbar}>
+            Contact
+          </Link>
+        </div>
+        <button className="menu-button" onClick={toggleNavbar}>
+          {openLinks ? <CloseIcon /> : <ReorderIcon />}
         </button>
       </div>
     </div>
