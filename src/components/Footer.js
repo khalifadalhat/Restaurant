e8dc6d85cd13dc9d04f733f2b55e5946d13802bb
khalifa <khalifadalhat@gmail.com>
2025-07-03 21:21:06 +0100

    Update footer copyright text to reflect 'Savory Haven. All rights reserved.'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
         <FacebookIcon />
         <LinkedInIcon />
         </div>
-        <p> &copy;2023 khalifadalhat.netlify.app</p>
+        <p> &copy;2023 Savory Haven. All rights reserved.</p>
     </div>
   )
 }
